40c6f1bdacd9fe91dcc13d0b0184d998fc56dc1d
Nick Koutrelakos <wkoutre@users.noreply.github.com>
2023-04-26 13:50:05 -0700

    Update babel.config.js

--- apps/expo/babel.config.js
+++ apps/expo/babel.config.js
@@ -19,7 +19,7 @@ module.exports = function (api) {
       [
         'transform-inline-environment-variables',
         {
-          include: 'TAMAGUI_TARGET',
+          include: ['TAMAGUI_TARGET'],
         },
       ],
     ],
